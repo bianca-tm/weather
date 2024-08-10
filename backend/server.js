8edaea72ee4f198a50b6a30b09bc9dfe4248edd0
bianca-tm <tech.btovalin@gmail.com>
2024-08-10 15:11:50 -0400

    backend changes for weather app integration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // what port localhost will be going to
 const PORT = 3000;
@@ -11,6 +15,31 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
+
+app.get('/api/weather', async (req, res) => {
+    const city = req.query.city || 'Dallas';
+    const apiKey = process.env.WEATHERSTACK_API_KEY;
+  
+    try {
+      const response = await axios.get(`http://api.weatherstack.com/current`, {
+        params: {
+          access_key: apiKey,
+          query: city
+        }
+      });
+
+      const data = response.data;
+      
+      res.json({
+        city: data.location.name,
+        country: data.location.country,
+        temperature: data.current.temperature,
+        weatherDescription: data.current.weather_descriptions[0]
+      });
+    } catch (error) {
+      res.status(500).json({ error: 'Unable to fetch weather data' });
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
